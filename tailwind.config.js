a8ce1818b9185c15b1b7185482147af35e7f815c
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-12 23:23:59 +0530

    add: tailwindcss theme extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '480px',
+      },
+      fontFamily: {
+        inter: ['Inter var', 'sans-serif'],
+      },
+      boxShadow: {
+        card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
+        cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
+      },
+    },
   },
   plugins: [],
 }
