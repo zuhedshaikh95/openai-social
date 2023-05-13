a7e49bb2e1572cb0beee905d37f51e0316e33e74
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-14 01:09:44 +0530

    fix: API size limit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  api: {
+    bodyParser: {
+      sizeLimit: '8mb' // Set desired value here
+    }
+  },
 }
 
 module.exports = nextConfig
