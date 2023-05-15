84402aaf6e3e0110fdbc3c95f803f7af5da59a86
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-15 21:32:03 +0530

    add: Download button

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export { default as Loader } from './Loader';
 export { default as EmptyState } from './EmptyState';
 export { default as Input } from './Input';
 export { default as Avatar } from './Avatar';
+export { default as DownloadButton } from './DownloadButton';
