9c8db6cfc218c60e9bdd9efbea3d52eb6484de4c
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-16 17:28:30 +0530

    fix: download image

--- utils/downloadImage.ts
+++ utils/downloadImage.ts
@@ -1,7 +1,8 @@
 import FileSaver from 'file-saver';
 
-const downloadImage = (_id: string, photo: string) => {
-    FileSaver.saveAs(photo, `download-${_id}.png`);
+const downloadImage = async (_id: string, photo: string) => {
+    const blob: Blob = await fetch(photo).then((res) => res.blob());
+    FileSaver.saveAs(blob, `download-${_id}.jpg`);
 }
 
 export default downloadImage;
