de11c002003ebda669c6452cbe80fbea2ee826f6
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-16 17:53:18 +0530

    fix: download image

--- utils/downloadImage.ts
+++ utils/downloadImage.ts
@@ -1,8 +1,18 @@
 import FileSaver from 'file-saver';
 
 const downloadImage = async (_id: string, photo: string) => {
-    const blob: Blob = await fetch(photo).then((res) => res.blob());
-    FileSaver.saveAs(blob, `download-${_id}.jpg`);
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open('GET', photo, true);
+    xhr.responseType = 'blob';
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            const blob = new Blob([xhr.response], { type: 'image/png' });
+            FileSaver.saveAs(blob, `social-ai-${_id}.jpg`);
+        }
+    }
+
+    xhr.send();
 }
 
 export default downloadImage;
