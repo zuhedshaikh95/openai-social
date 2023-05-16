7f925b4de2e64d1bf8d67f932962165a950556aa
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-16 17:15:57 +0530

    fix: file-saver format

--- utils/downloadImage.ts
+++ utils/downloadImage.ts
@@ -1,7 +1,7 @@
 import FileSaver from 'file-saver';
 
 const downloadImage = (_id: string, photo: string) => {
-    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+    FileSaver.saveAs(photo, `download-${_id}.png`);
 }
 
 export default downloadImage;
