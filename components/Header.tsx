f49b3327d94d8888f47397ffd60605dd62e49539
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-18 20:52:12 +0530

    fix: Removed create button

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,19 +4,13 @@ import { logo } from "@/public/assets";
 
 const Header = () => {
   return (
-    <header className="w-full flex justify-between items-center bg-white px-4 py-4 sm:px-8 border-b border-b-[#e6ebf4]">
+    <header className="w-fullbg-white px-4 py-4 sm:px-8 border-b border-b-[#e6ebf4]">
       <Link href="/">
         <Image
           src={logo}
           alt="logo"
           className="w-28 object-contain cursor-pointer"
         />
-      </Link>
-
-      <Link href="/create-post">
-        <button className="font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
-          Create
-        </button>
       </Link>
     </header>
   );
